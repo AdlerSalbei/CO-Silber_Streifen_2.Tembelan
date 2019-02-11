a8ddf0a1052c26c5aab86fc11780b0e655cba7ce
Salbei <salbei@gruppe-adler.de>
2019-02-11 16:22:45 +0100

    Update missionInfo.hpp

--- USER/missionInfo.hpp
+++ USER/missionInfo.hpp
@@ -2,9 +2,9 @@
 *   Legt allgemeine Information über die Mission fest.
 */
 
-author = "B4d Horse, Odyssey & Salbei für Gruppe Adler";                         // Missionsersteller
-onLoadName = "COOP Silber Streifen 2";                                          // Name der Mission
-onLoadMission = "";                                                             // Beschreibung der Mission (wird im Ladebildschirm unterhalb des Ladebildes angezeigt)
-loadScreen = "data\loadpic.paa";                                                // Ladebild
-overviewPicture = "";                                                           // Bild, das in der Missionsauswahl angezeigt wird
-overviewText = "";                                                              // Text, der in der Missionsauswahl angezeigt wird
+author = "B4d Horse, Odyssey & Salbei für Gruppe Adler";                             // Missionsersteller
+onLoadName = "COOP Silber Streifen 2";                                               // Name der Mission
+onLoadMission = "Der Feind meines Feindes ist nur ein anderer Typ mit einer Waffe";  // Beschreibung der Mission (wird im Ladebildschirm unterhalb des Ladebildes angezeigt)
+loadScreen = "data\loadpic.paa";                                                     // Ladebild
+overviewPicture = "";                                                                // Bild, das in der Missionsauswahl angezeigt wird
+overviewText = "";                                                                   // Text, der in der Missionsauswahl angezeigt wird
