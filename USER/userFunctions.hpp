cbb361aa734829fe17246c891fac9c2daf4ab734
Salbei <salbei@gruppe-adler.de>
2019-02-11 12:05:50 +0100

    removed introCam, added better Explosion

--- USER/userFunctions.hpp
+++ USER/userFunctions.hpp
@@ -2,3 +2,9 @@
 *   Hier können eigene Funktionen eingebunden werden.
 *   Ist in CfgFunctions included.
 */
+class GRAD_SS {
+    class common {
+        file = "USER\functions";
+        class addToWave {};
+    };
+};
--- USER/userSounds.hpp
+++ USER/userSounds.hpp
@@ -2,3 +2,14 @@
 *   Hier können eigene Sounds eingebunden werden.
 *   Ist in CfgSounds included.
 */
+
+class bombExplosion	{
+    name = "bombExplosion";
+    sound[] = {"\sound\bombExplosion.ogg", db+20, 1};
+    titles[] = {1, ""};
+};
+class bombExplosionEcho {
+    name = "bombExplosionEcho";
+    sound[] = {"\sound\bombExplosionEcho.ogg", db+5, 1};
+    titles[] = {1, ""};
+};
